f0b10e7bd2a28ec22bc93a469b62ebdab18a6df3
AlexandruOlteanu <alexandruolteanu2001@gmail.com>
2023-07-22 19:32:54 +0300

    Removed unnecessary import

--- nevsquare/src/main/java/com/nevsquare/api/NevsquareService.java
+++ nevsquare/src/main/java/com/nevsquare/api/NevsquareService.java
@@ -5,7 +5,6 @@ import com.nevsquare.api.domain.response.NevsquareCommunicationResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
